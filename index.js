0fc9c042fcd9d2511d92b6cad3289f001c73b2bc
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2023-05-23 01:49:36 -0500

    mongodb database url added

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const io = require("socket.io")(server);
 
 global.io = io;
 
-dbConnect("mongodb://localhost:27017")
+dbConnect(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.eo3neab.mongodb.net/test`
+)
   .then(() => {
     server.listen(port, () => {
       console.log(`server is running port - ${port}`);
